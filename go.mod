be4208932247557a33182fc60603381b1b4f46b1
bruckins <karelnebojsa@seznam.cz>
2025-08-23 17:27:57 +0200

    bp validation

--- go.mod
+++ go.mod
@@ -2,13 +2,16 @@ module github.com/k8shell-io/common
 
 go 1.24.5
 
-require github.com/rs/zerolog v1.34.0
+require (
+	github.com/go-playground/validator/v10 v10.27.0
+	github.com/rs/zerolog v1.34.0
+	gopkg.in/yaml.v3 v3.0.1
+)
 
 require (
 	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/go-playground/validator/v10 v10.27.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
--- models/blueprint.go
+++ models/blueprint.go
@@ -1,5 +1,13 @@
 package models
 
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/go-playground/validator/v10"
+	"gopkg.in/yaml.v3"
+)
+
 // Blueprint represents a single blueprint configuration
 type Blueprint struct {
 	Name            string              `yaml:"name" validate:"required,min=1,max=30"`
@@ -89,4 +97,47 @@ type Storage struct {
 type Repo struct {
 	Name  string `yaml:"name" validate:"required"`
 	Owner string `yaml:"owner" validate:"required"`
+}
+
+func ValidateCustomBlueprint(blueprintYAML []byte) []string {
+	var fullYAML map[string]interface{}
+	if err := yaml.Unmarshal(blueprintYAML, &fullYAML); err != nil {
+		return []string{
+			fmt.Sprintf("Invalid YAML format: %v", err),
+		}
+	}
+
+	var blueprintData, exists = fullYAML["blueprint"]
+	if !exists {
+		return []string{
+			"Blueprint data is missing in the YAML",
+		}
+	}
+
+	blueprintOnlyYAML, err := yaml.Marshal(blueprintData)
+	if err != nil {
+		return []string{
+			fmt.Sprintf("Failed to process blueprint data: %v", err),
+		}
+	}
+
+	var customBp CustomBlueprint
+	decoder := yaml.NewDecoder(bytes.NewReader(blueprintOnlyYAML))
+	decoder.KnownFields(true)
+	if err := decoder.Decode(&customBp); err != nil {
+		return []string{
+			fmt.Sprintf("Failed to decode blueprint: %v", err),
+		}
+	}
+
+	validate := validator.New()
+	if err := validate.Struct(customBp); err != nil {
+		var validationErrors []string
+		for _, err := range err.(validator.ValidationErrors) {
+			validationErrors = append(validationErrors,
+				fmt.Sprintf("Field '%s' failed validation: %s", err.Field(), err.Tag()))
+		}
+		return validationErrors
+	}
+	return nil
 }
