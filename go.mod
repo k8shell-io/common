fd6394408c910197a0af1a50bfb10dc51192cb5b
bruckins <karelnebojsa@seznam.cz>
2025-10-10 21:55:05 +0200

    grpc

--- go.mod
+++ go.mod
@@ -38,10 +38,12 @@ require (
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	golang.org/x/arch v0.8.0 // indirect
-	golang.org/x/crypto v0.37.0 // indirect
-	golang.org/x/net v0.38.0 // indirect
-	golang.org/x/sync v0.13.0 // indirect
-	golang.org/x/sys v0.32.0 // indirect
-	golang.org/x/text v0.24.0 // indirect
-	google.golang.org/protobuf v1.34.2 // indirect
+	golang.org/x/crypto v0.40.0 // indirect
+	golang.org/x/net v0.42.0 // indirect
+	golang.org/x/sync v0.16.0 // indirect
+	golang.org/x/sys v0.34.0 // indirect
+	golang.org/x/text v0.27.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250804133106-a7a43d27e69b // indirect
+	google.golang.org/grpc v1.76.0 // indirect
+	google.golang.org/protobuf v1.36.6 // indirect
 )
--- pkg/grpc/grpc.go
+++ pkg/grpc/grpc.go
@@ -1,5 +1,47 @@
 package grpc
 
-type GrpcConfig struct {
-	Port int `yaml:"port"`
+import (
+	"fmt"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+)
+
+type Config struct {
+	Port      int    `yaml:"port"`
+	enableTLS bool   `yaml:"enableTLS"`
+	certFile  string `yaml:"certFile"`
+	keyFile   string `yaml:"keyFile"`
+}
+
+type Server struct {
+	Config     *Config
+	listener   net.Listener
+	grpcServer *grpc.Server
+}
+
+// Start starts the gRPC server with optional TLS
+func NewServer(config *Config) (*Server, error) {
+	server := &Server{
+		Config: config,
+	}
+
+	var err error
+	server.listener, err = net.Listen("tcp", fmt.Sprintf(":%d", config.Port))
+	if err != nil {
+		return nil, fmt.Errorf("failed to listen: %w", err)
+	}
+
+	if config.enableTLS {
+		creds, err := credentials.NewServerTLSFromFile(config.certFile, config.keyFile)
+		if err != nil {
+			return nil, fmt.Errorf("failed to load TLS credentials: %w", err)
+		}
+		server.grpcServer = grpc.NewServer(grpc.Creds(creds))
+	} else {
+		server.grpcServer = grpc.NewServer()
+	}
+
+	return server, nil
 }
