39c543e060d71d6a3b285fe5d7d5ed3f71e62549
bruckins <karelnebojsa@seznam.cz>
2026-04-17 15:53:23 +0200

    podman splash removed from config

--- pkg/api/client/k8shelld/config.go
+++ pkg/api/client/k8shelld/config.go
@@ -1,8 +1,6 @@
 package k8shelld
 
 import (
-	"time"
-
 	"github.com/k8shell-io/common/pkg/gapi"
 )
 
@@ -18,13 +16,9 @@ type Identity struct {
 type Config struct {
 	System           System           `yaml:"system"`
 	Identity         Identity         `yaml:"identity"`
-	Splash           string           `yaml:"splash,omitempty"`
 	TerminateOrphans TerminateOrphans `yaml:"terminateOrphans"`
 	ReapZombies      ReapZombies      `yaml:"reapZombies"`
-	Podman           PodmanConfig     `yaml:"podman"`
 	InitScriptsDir   string           `yaml:"initScriptsDir,omitempty"`
-	EnableApps       bool             `yaml:"enableApps"`
-	Apps             Apps             `yaml:"apps,omitempty"`
 }
 
 // System represents the general system configuration.
@@ -51,31 +45,4 @@ type TerminateOrphans struct {
 // ReapZombies represents the configuration for the reap-zombies feature.
 type ReapZombies struct {
 	Enabled bool `yaml:"enabled"`
-}
-
-// PodmanConfig holds Podman runtime settings written into config.yaml.
-// This is distinct from the blueprint's Podman spec (models.Podman).
-type PodmanConfig struct {
-	Enabled                 bool `yaml:"enabled"`
-	CreateDockerSockSymlink bool `yaml:"createDockerSockSymlink"`
-}
-
-// Apps is a map of app name to AppSpec, matching k8shelld's Apps type.
-type Apps map[string]*AppSpec
-
-// AppSpec represents the specification for a single app in k8shelld.
-// Note: the Enabled field is part of the blueprint's AppSpec, not the config file spec.
-type AppSpec struct {
-	Name              string        `yaml:"name,omitempty"`
-	Binary            string        `yaml:"binary"`
-	VersionCmd        []string      `yaml:"versionCmd,omitempty"`
-	VersionRegex      string        `yaml:"versionRegex,omitempty"`
-	Install           string        `yaml:"install,omitempty"`
-	Start             []string      `yaml:"start,omitempty"`
-	Listen            int           `yaml:"listen,omitempty"`
-	RestartPolicy     string        `yaml:"restartPolicy,omitempty"`
-	MaxRestartBackoff time.Duration `yaml:"maxRestartBackoff,omitempty"`
-	InstallAsRoot     bool          `yaml:"installAsRoot"`
-	AutoStart         bool          `yaml:"autoStart"`
-	Protocol          string        `yaml:"protocol,omitempty"`
 }
