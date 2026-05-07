50eac8451c28b5394782fd3526dd1c478b28709b
bruckins <karelnebojsa@seznam.cz>
2026-05-07 16:16:58 +0200

    identity removed from config

--- pkg/api/client/k8shelld/config.go
+++ pkg/api/client/k8shelld/config.go
@@ -4,18 +4,9 @@ import (
 	"github.com/k8shell-io/common/pkg/gapi"
 )
 
-// Identity holds the configuration required to load and verify the workspace
-// identity JWT at startup and during periodic renewal checks.
-type Identity struct {
-	TokenPath     string `yaml:"tokenPath"`
-	PublicKeyPath string `yaml:"publicKeyPath"`
-	SigningMethod string `yaml:"signingMethod"`
-}
-
 // Config is the root structure of k8shelld's config.yaml.
 type Config struct {
 	System           System           `yaml:"system"`
-	Identity         Identity         `yaml:"identity"`
 	SaToken          SaToken          `yaml:"saToken"`
 	TerminateOrphans TerminateOrphans `yaml:"terminateOrphans"`
 	ReapZombies      ReapZombies      `yaml:"reapZombies"`
